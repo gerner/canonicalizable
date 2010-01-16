78d8815747dadb6f76d0a1c7c7e7d4963f61526b
Nick Gerner <nick@nick-laptop.(none)>
2010-01-16 15:57:20 -0800

    reorganizing configuration.php and includes.php so only config is in config and includes includes that

---
+++ includes.php
@@ -0,0 +1,11 @@
+<?php
+require_once("configuration.php");
+
+/*
+ * require everything we need in once place, here
+ */
+require_once("parallel_request.php");
+require_once("models/canonical_candidate.php");
+require_once("models/canonicalizing_homepage.php");
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
 <?php 
-require_once("configuration.php"); 
+require_once("includes.php"); 
 require_once("controllers/canonicalizable.php");
 ?>
 <html>
