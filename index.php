0e3458299ef2f8173e8a7e8f043aadb0e40c1ce9
Nick Gerner <nick@nick-laptop.(none)>
2010-01-17 21:54:30 -0800

    adding google analytics tracking

--- index.php
+++ index.php
@@ -91,5 +91,19 @@ list($isgood, $candidate, $message) = $candidate_info;?>
 </div>
 
 </div>
+
+<?php if(GG_ANALYTICS_ID) {?>
+<script type="text/javascript"> 
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script> 
+<script type="text/javascript"> 
+	try {
+		var pageTracker = _gat._getTracker("<?php echo GG_ANALYTICS_ID;?>");
+		// Cookied already: 
+		pageTracker._trackPageview();
+	} catch(err) {}
+</script>
+<?php }?>
 </body>
 </html>
